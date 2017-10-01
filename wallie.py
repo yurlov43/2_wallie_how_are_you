d5dc98858b15398a1fae225f971ac94fb8be0d86
yurlov43 <yurlov43@yandex.ru>
2017-10-01 19:47:44 +0300

    исправлен баг

--- wallie.py
+++ wallie.py
@@ -1,4 +1,4 @@
-import random
+﻿import random
 
 
 def get_wallie_action():
@@ -17,4 +17,4 @@ def get_wallie_action():
 
 if __name__ == '__main__':
     action = get_wallie_action()
-    print action
+    print(action)
